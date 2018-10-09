1d85c1826e62d9c2fa14f4260385f4bbf1f08b16
MartGon <gudo35@gmail.com>
2018-10-09 13:23:12 +0200

    Implemented cryptographically strong random generator

--- p3/src/DvdCrypt.cpp
+++ p3/src/DvdCrypt.cpp
@@ -1,31 +1,26 @@
 #include "DvdCrypt.h"
 
+
+// Config
+bool debug = true;
+
 int main(void)
 {
-	unsigned char* key = (unsigned char*)"*F)J@NcRfUjXn2r";
-	unsigned char* iv = (unsigned char*)"McQfTjWnZr4u7x!";
-	unsigned char* message = (unsigned char*)"HolaHolaHola123";
-	unsigned char* ciphertext = new unsigned char[128];
-	unsigned char decryptedtext[128];
-	int ciphertext_len = 0;
-	int decryptedtext_len = 0;
+	initRandomGenerator();
 
-	ciphertext_len = aes_encrypt_func(key, iv, message, strlen((char*)message), ciphertext);
+	// Debug ciphering functions
+	test_ciphering();
 
-	std::cout << (char*)ciphertext << "\n";
-
-	decryptedtext_len = aes_decrypt_func(ciphertext, ciphertext_len, key, iv, decryptedtext);
-
-	decryptedtext[decryptedtext_len] = '\0';
-
-	std::cout << (char*)decryptedtext << "\n";
+	generateBinaryTree(4);
 
 	getchar();
 
 	return 0;
 }
 
-int32_t aes_encrypt_func(const unsigned char* key, const unsigned char* iv, unsigned char* plaintext, int32_t plaintext_len, unsigned char* ciphertext)
+// Cipher fuctions
+
+int32_t aes_encrypt_func(unsigned char* plaintext, int32_t plaintext_len, const unsigned char* key, const unsigned char* iv, unsigned char* ciphertext)
 {
 	// We create the encrypting object
 	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
@@ -81,4 +76,111 @@ int32_t aes_decrypt_func(unsigned char * ciphertext, int ciphertext_len, unsigne
 	EVP_CIPHER_CTX_free(ctx);
 
 	return plaintext_len;
+}
+
+void initRandomGenerator()
+{
+	// Variables
+	unsigned long err = 0;
+	int rc = 0;
+
+	// Load engines
+	ENGINE_load_builtin_engines();
+
+	// Get the random generator engine
+	ENGINE* eng = ENGINE_by_id("rdrand");
+
+	if (!eng)
+		log("Error while loading the random generator engine \n");
+
+	// Initializing this engine
+	rc = ENGINE_init(eng);
+	err = ERR_get_error();
+
+	if (!rc)
+		std::cout << "ENGINE_init failed " + std::to_string(err) + "\n";
+
+	// Setting default method rand
+	rc = ENGINE_set_default(eng, ENGINE_METHOD_RAND);
+	err = ERR_get_error();
+
+	if (!rc)
+		std::cout << "ENGINE_set_default failed " + std::to_string(err) + "\n";
+
+	return;
+}
+
+// Key generation
+unsigned char * generateRandomKey(int32_t size)
+{
+	unsigned char* buffer = new unsigned char[size];
+	int rc = RAND_bytes(buffer, size);
+
+	if (rc != 1)
+		log("Error while generating a random key");
+
+	// Cut char string
+	buffer[size] = '\0';
+
+	return buffer;
+}
+
+// Debug
+void test_ciphering()
+{
+	unsigned char* key = (unsigned char*)"*F)J@NcRfUjXn2r";
+	key = generateRandomKey(16);
+	unsigned char* iv = (unsigned char*)"McQfTjWnZr4u7x!";
+	unsigned char* message = (unsigned char*)"HolaHolaHola123";
+	unsigned char* ciphertext = new unsigned char[128];
+	unsigned char decryptedtext[128];
+	int ciphertext_len = 0;
+	int decryptedtext_len = 0;
+
+	ciphertext_len = aes_encrypt_func(message, strlen((char*)message), key, iv, ciphertext);
+	ciphertext[ciphertext_len] = '\0';
+
+	std::cout << (char*)ciphertext << "\n";
+
+	decryptedtext_len = aes_decrypt_func(ciphertext, ciphertext_len, key, iv, decryptedtext);
+
+	decryptedtext[decryptedtext_len] = '\0';
+
+	std::cout << (char*)decryptedtext << "\n";
+}
+
+void log(std::string str)
+{
+	if (debug)
+		std::cout << str.c_str();
+}
+
+// Tree functions
+Tree generateBinaryTree(int32_t levels)
+{
+	int32_t size = calculateTreeSize(levels);
+	log("Tree size is " + std::to_string(size) + "\n");
+
+	return Tree();
+}
+
+int32_t calculateTreeSize(int32_t levels)
+{
+	return pow(2, levels) - 1;
+}
+
+// Classes
+int16_t Node::last_id = 0;
+
+Node::Node(Node* parent, Node* sibling, Key key)
+{
+	id = get_id();
+	this->parent = parent;
+	this->sibling = sibling;
+	this->key = key;
+}
+
+int32_t Node::get_id()
+{
+	return last_id++;
 }
--- p3/src/DvdCrypt.h
+++ p3/src/DvdCrypt.h
@@ -1,10 +1,65 @@
-#include <openssl/conf.h>
+// OpenSSL
 #include <openssl/evp.h>
 #include <openssl/err.h>
+#include <openssl/engine.h>
+#include <openssl/rand.h>
+
+// C Standard Libs
 #include <string.h>
-#include <iostream>
 #include <stdio.h>
 
-int32_t aes_encrypt_func(const unsigned char* key, const unsigned char* iv, unsigned char* plaintext, int32_t plaintext_len, unsigned char* ciphertext);
+// C++ Standard Libs
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <string>
+
+// Forward Declaration
+class Node;
+
+// Utilities
+using Tree = std::vector<Node*>;
+using Key = unsigned char*;
+
+// Cipher fuctions
+int32_t aes_encrypt_func(unsigned char* plaintext, int32_t plaintext_len, const unsigned char* key, const unsigned char* iv, unsigned char* ciphertext);
 
 int32_t aes_decrypt_func(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext);
+
+// Key generation
+void initRandomGenerator();
+
+unsigned char* generateRandomKey(int32_t size);
+
+// Debug functions
+void test_ciphering();
+
+void log(std::string str);
+
+// Useful classes
+class Node
+{
+public:
+	// Constructor
+	Node(Node* parent, Node* sibling, Key key);
+
+	// Node Utility variables
+	int32_t id = 0;
+	Node* parent = nullptr;
+	Node* sibling = nullptr;
+	std::pair<Node*, Node*> children;
+
+	// Key
+	Key key = nullptr;
+
+private:
+	// Id measures
+	static int16_t last_id;
+	int32_t get_id();
+};
+
+// Tree functions
+Tree generateBinaryTree(int32_t levels);
+
+int32_t calculateTreeSize(int32_t levels);
